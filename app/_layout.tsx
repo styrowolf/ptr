36134b0c72a651d733215b42ce0d5a4ab824cec9
Oğuz Kurt <kurt.oguz@outlook.com>
2024-09-29 11:12:34 -0400

    fix: i18n not working when doing archive builds

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,41 @@ import React, { useEffect, useReducer, useState } from "react";
 import { useFonts } from "expo-font";
 import { IBMPlexMono_700Bold_Italic } from "@expo-google-fonts/ibm-plex-mono";
 import { ToplasAPICache, ToplasPreferences } from "./storage";
+// i18n imports start
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import RNLanguageDetector from "@os-team/i18next-react-native-language-detector";
+import "intl-pluralrules";
+import enTranslation from "./locales/en/translation.json";
+import trTranslation from "./locales/tr/translation.json";
+// i18n imports end
 
 SplashScreen.preventAutoHideAsync();
 
 ToplasAPICache.clear();
 
 MapLibreGL.setAccessToken(null);
+
+// i18n init start
+i18n
+  .use(RNLanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      tr: {
+        translation: trTranslation,
+      },
+    },
+    supportedLngs: ["en", "tr"],
+    fallbackLng: "en",
+    //interpolation: {
+    //    escapeValue: false,
+    //},
+  });
+// i18n init end
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
